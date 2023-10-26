46b49e57b15463d8a9840c98ea6999812e46585f
areebaqamar021 <areebaqamar021@gmail.com>
2023-10-26 12:33:14 +0500

    lets go

--- colorChanger/index.js
+++ colorChanger/index.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 
 buttons.forEach(function (button) {
@@ -6,5 +6,17 @@ buttons.forEach(function (button) {
     button.addEventListener('click', function(e) {
         console.log(e);
         console.log(e.target);
+        if(e.target.id === 'gray'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }
     })
 })
