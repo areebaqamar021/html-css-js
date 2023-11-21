d7571a2f2b04940c5d2d5cdf665e05f72eda70bc
areebaqamar021 <areebaqamar021@gmail.com>
2023-11-21 13:20:05 +0500

    practice

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,5 +1,20 @@
-console.log('areeba');
+// console.log('areeba');
 
-let firstName = 'john'
-let lastName = "doe"
-console.log(`my name is ${firstName} ${lastName}`);
+// let firstName = 'john'
+// let lastName = "doe"
+// console.log(`my name is ${firstName} ${lastName}`);
+
+// console.log(firstName.slice(0,2));
+
+// console.log(lastName.includes('e'));
+
+// let arrNames = ['abc', 'def', 'ghi'];
+// console.log(arrNames)
+// for(let i=0; i<arrNames.length; i++){
+//     console.log(arrNames[i])
+// }
+
+let names = function(name='Areeba', fatherName= 'Qamar'){
+    console.log(`My name is ${name} and my father name is ${fatherName}`)
+}
+names()
