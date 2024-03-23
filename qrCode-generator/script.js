7c1ccf85e8ce2e70b4a7357fccd11a1cd2a476b1
areebaqamar021 <areebaqamar021@gmail.com>
2024-03-23 22:16:09 +0500

    html css done

---
+++ qrCode-generator/script.js
@@ -0,0 +1,7 @@
+let qrText = document.getElementById('imgBox');
+let imgBox = document.getElementById('imgBox');
+let qrImage = document.getElementById('qrImage');
+
+function  generateQR() {
+    qrImage.src = ' https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
+}
