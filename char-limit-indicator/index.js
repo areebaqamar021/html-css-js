8dc55f6ae255752fc11d3bfec3d561ad763d4e02
areebaqamar021 <areebaqamar021@gmail.com>
2024-04-11 22:08:04 +0500

    now

---
+++ char-limit-indicator/index.js
@@ -0,0 +1,9 @@
+const textArea = document.getElementById('txt');
+const progressBar = document.getElementById('progress-bar');
+const remChar = document.getElementById('remaining-chars');
+
+function charCounter(inputfield){
+    const maxLength = inputfield.getAttribute('maxlength');
+    const currentLength = inputfield.value.length;
+    
+}
