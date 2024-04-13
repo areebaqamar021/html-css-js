205ff18ccb01e511a9b90afe92ad81da142dc465
areebaqamar021 <areebaqamar021@gmail.com>
2024-04-13 21:20:07 +0500

    added if else condition

--- char-limit-indicator/index.js
+++ char-limit-indicator/index.js
@@ -10,5 +10,14 @@ function charCounter(inputfield){
     progressBar.style.width = `${progressWidth}%`;
     remChar.style.display = 'none';
 
-    
+    if (progressWidth <= 60) {
+        progressBar.style.backgroundColor = "rgb(19, 160, 19)";
+      } else if (progressWidth > 60 && progressWidth < 85) {
+        progressBar.style.backgroundColor = "rgb(236, 157, 8)";
+      } else {
+        progressBar.style.backgroundColor = "rgb(241, 9, 9)";
+        remChars.innerHTML = `${maxLength - currentLength} characters left`;
+        remChars.style.display = "block";
+      }
+    }
 }
