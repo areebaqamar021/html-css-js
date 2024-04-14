334b068f2d89beb4f7186e8669aee5ac2aa82af3
areebaqamar021 <areebaqamar021@gmail.com>
2024-04-14 23:10:58 +0500

    completed

--- char-limit-indicator/index.js
+++ char-limit-indicator/index.js
@@ -1,22 +1,26 @@
-const textArea = document.getElementById('txt');
-const progressBar = document.getElementById('progress-bar');
-const remChar = document.getElementById('remaining-chars');
+const textareas = document.querySelectorAll(".txt");
+const progressBars = document.querySelectorAll(".progress-bar");
+const remCharsElems = document.querySelectorAll(".remaining-chars");
 
-function charCounter(inputfield){
-    const maxLength = inputfield.getAttribute('maxlength');
-    const currentLength = inputfield.value.length;
-    const progressWidth = (currentLength / maxLength) * 100;
+function charCounter(inputField, progress, warningTxt) {
+  const maxLength = inputField.getAttribute("maxlength");
+  const currentLength = inputField.value.length;
 
-    progressBar.style.width = `${progressWidth}%`;
-    remChar.style.display = 'none';
+  const progressWidth = (currentLength / maxLength) * 100;
+  progress.style.width = `${progressWidth}%`;
+  warningTxt.style.display = "none";
 
-    if (progressWidth <= 60) {
-        progressBar.style.backgroundColor = "rgb(19, 160, 19)";
-      } else if (progressWidth > 60 && progressWidth < 85) {
-        progressBar.style.backgroundColor = "rgb(236, 157, 8)";
-      } else {
-        progressBar.style.backgroundColor = "rgb(241, 9, 9)";
-        remChars.innerHTML = `${maxLength - currentLength} characters left`;
-        remChars.style.display = "block";
-      }
-    }
+  if (progressWidth <= 60) {
+    progress.style.backgroundColor = "rgb(19, 160, 19)";
+  } else if (progressWidth > 60 && progressWidth < 85) {
+    progress.style.backgroundColor = "rgb(236, 157, 8)";
+  } else {
+    progress.style.backgroundColor = "rgb(241, 9, 9)";
+    warningTxt.innerHTML = `${maxLength - currentLength} characters left`;
+    warningTxt.style.display = "block";
+  }
+}
+
+textareas.forEach((textarea, i) => {
+  textarea.oninput = () => charCounter(textarea, progressBars[i], remCharsElems[i]);
+})
