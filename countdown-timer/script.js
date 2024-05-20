9ad04f14db5fcaf14906dad66f5f826a42168242
areebaqamar021 <areebaqamar021@gmail.com>
2024-05-20 14:56:42 +0500

    Implemented countdown timer feature using HTML, CSS, and JavaScript

---
+++ countdown-timer/script.js
@@ -0,0 +1,22 @@
+// Set the date we're counting down to
+const birthday = new Date('November 12, 2024 00:00:00').getTime();
+
+const countdown = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = birthday - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdown);
+        document.getElementById('countdown').innerHTML = '<h2>Happy Birthday!</h2>';
+    }
+}, 1000);
